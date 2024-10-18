a397ac8f6214b87258fead7c954c5d0aa6bc80e6
rohitbalaji141 <rohitbalajim30@gmail.com>
2024-10-18 19:54:37 +0530

    Update main.py
    cleared the errors

--- python/color_detection_tool/main.py
+++ python/color_detection_tool/main.py
@@ -3,70 +3,70 @@ import numpy as np
 import pandas as pd
 import argparse
 
-#Creating argument parser to take image path from command line
+# Creating argument parser to take image path from command line
 ap = argparse.ArgumentParser()
 ap.add_argument('-i', '--image', required=True, help="Image Path")
 args = vars(ap.parse_args())
 img_path = args['image']
 
-#reading the image with opencv
+# Reading the image with OpenCV
 img = cv2.imread(img_path)
 
-#declaring global variables (are used later on)
+# Declaring global variables (used later)
 clicked = False
 r = g = b = xpos = ypos = 0
 
-#reading csv file with pandas and giving names to each column
-index=["color","color_name","hex","R","G","B"]
+# Reading CSV file with pandas and giving names to each column
+index = ["color", "color_name", "hex", "R", "G", "B"]
 csv = pd.read_csv('colors.csv', names=index, header=None)
 
-#function to calculate minimum distance from all colors and get the most matching color
-def getColorName(R,G,B):
-    minimum = 10000
+# Function to calculate minimum distance from all colors and get the most matching color
+def getColorName(R, G, B):
+    minimum = float('inf')
+    cname = ""
     for i in range(len(csv)):
-        d = abs(R- int(csv.loc[i,"R"])) + abs(G- int(csv.loc[i,"G"]))+ abs(B- int(csv.loc[i,"B"]))
-        if(d<=minimum):
+        d = abs(R - int(csv.loc[i, "R"])) + abs(G - int(csv.loc[i, "G"])) + abs(B - int(csv.loc[i, "B"]))
+        if d < minimum:
             minimum = d
-            cname = csv.loc[i,"color_name"]
+            cname = csv.loc[i, "color_name"]
     return cname
 
-#function to get x,y coordinates of mouse double click
-def draw_function(event, x,y,flags,param):
+# Function to get x, y coordinates of mouse double click
+def draw_function(event, x, y, flags, param):
+    global b, g, r, xpos, ypos, clicked
     if event == cv2.EVENT_LBUTTONDBLCLK:
-        global b,g,r,xpos,ypos, clicked
         clicked = True
         xpos = x
         ypos = y
-        b,g,r = img[y,x]
+        b, g, r = img[y, x]
         b = int(b)
         g = int(g)
         r = int(r)
-       
+
+# Setting up the OpenCV window and mouse callback
 cv2.namedWindow('image')
-cv2.setMouseCallback('image',draw_function)
+cv2.setMouseCallback('image', draw_function)
 
-while(1):
+while True:
+    cv2.imshow("image", img)
+    if clicked:
+        # Filling rectangle with the color
+        cv2.rectangle(img, (20, 20), (750, 60), (b, g, r), -1)
 
-    cv2.imshow("image",img)
-    if (clicked):
-   
-        #cv2.rectangle(image, startpoint, endpoint, color, thickness)-1 fills entire rectangle 
-        cv2.rectangle(img,(20,20), (750,60), (b,g,r), -1)
-
-        #Creating text string to display( Color name and RGB values )
-        text = getColorName(r,g,b) + ' R='+ str(r) +  ' G='+ str(g) +  ' B='+ str(b)
+        # Creating text string to display (Color name and RGB values)
+        text = f"{getColorName(r, g, b)} R={r} G={g} B={b}"
         
-        #cv2.putText(img,text,start,font(0-7),fontScale,color,thickness,lineType )
-        cv2.putText(img, text,(50,50),2,0.8,(255,255,255),2,cv2.LINE_AA)
+        # Displaying text on image
+        cv2.putText(img, text, (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 255, 255), 2, cv2.LINE_AA)
 
-        #For very light colours we will display text in black colour
-        if(r+g+b>=600):
-            cv2.putText(img, text,(50,50),2,0.8,(0,0,0),2,cv2.LINE_AA)
-            
-        clicked=False
+        # For very light colors, display text in black color
+        if (r + g + b) >= 600:
+            cv2.putText(img, text, (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 0), 2, cv2.LINE_AA)
 
-    #Break the loop when user hits 'esc' key    
-    if cv2.waitKey(20) & 0xFF ==27:
+        clicked = False
+
+    # Break the loop when the user hits the 'esc' key    
+    if cv2.waitKey(20) & 0xFF == 27:
         break
-    
+
 cv2.destroyAllWindows()
