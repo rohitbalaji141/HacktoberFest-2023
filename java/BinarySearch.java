29198aa705e6d39cefd9fc5dc5a733503012ee44
rohitbalaji141 <rohitbalajim30@gmail.com>
2024-10-18 19:21:05 +0530

    Update BinarySearch.java
    cleared the problems

--- java/BinarySearch.java
+++ java/BinarySearch.java
@@ -1,24 +1,27 @@
 public class BinarySearch {
-    public static void main(String args[]) {
+    public static void main(String[] args) {
+        int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9}; // Sorted array
 
-        int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-
-        int low = 0, high = (arr.length - 1);
-
-        int num = 6;
+        int low = 0, high = arr.length - 1;
+        int num = 6; // Number to search for
+        boolean found = false; // Flag to indicate if the number is found
 
         while (low <= high) {
-
             int mid = (low + high) / 2;
 
             if (arr[mid] == num) {
-                System.out.println("At: " + (mid + 1));
+                System.out.println("Number found at index: " + mid);
+                found = true; // Set flag to true
                 break;
             } else if (arr[mid] < num) {
-                low = mid + 1;
-            } else if (arr[mid] > num) {
-                high = mid - 1;
+                low = mid + 1; // Move to the right half
+            } else {
+                high = mid - 1; // Move to the left half
             }
+        }
+
+        if (!found) {
+            System.out.println("Number not found in the array.");
         }
     }
 }
